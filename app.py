b577d82a3cbc564ba54744699f8f739e0175683d
aravgoyal <aravgoyal@outlook.com>
2024-08-05 15:28:11 -0400

    recent changes

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-@app.route('/api/', methods=["GET", "POST"])
+@app.route('/api/transfermarkt', methods=["GET", "POST"])
 def take_it():
     data = request.json()
-    return jsonify("elp elp elp")
+    return "elp elp elp"
 
 if __name__ == "__main__":
     app.run(port=3000)
